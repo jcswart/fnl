b25ef53d33f1b8e13acbf22d949e0c888023a0b7
Jarrod Swart <jcswart@gmail.com>
2016-12-31 19:11:18 -0500

    Comments, rename.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::io::BufRead;
 use std::io::Write;
 
 fn main() {
+    //
+    // Check the args.
+    //
     if e::args().len() == 1 {
         let basename = e::args().next().unwrap();
         let usage    = format!("Usage: {} [ COLUMNS... ]\n\nFunnel columns of data from stdin.\n\tCOLUMNS\t%1, %2, etc. Like awk: {{ print $1, $2; }}\n\n\tEx: {0} %1 %2", basename);
@@ -11,6 +14,9 @@ fn main() {
         std::process::exit(1);
     }
 
+    //
+    // Process the args.
+    //
     let mut cols = vec![];
     for a in e::args() {
         let   chars: Vec<char> = a.chars().collect();
@@ -20,6 +26,9 @@ fn main() {
         };
     }
 
+    //
+    // Iterate over stdin.
+    //
     let stdin = io::stdin();
     for l in stdin.lock().lines() {
         let line              = l.unwrap();
@@ -40,19 +49,19 @@ fn main() {
 
 fn intersperse_tab(xs: Vec<&str>) -> Vec<String> {
     let mut zs: Vec<String> = vec![];
-    let mut peekers         = xs.iter().peekable();
-    let mut not_first = false;
+    let mut coll            = xs.iter().peekable();
+    let mut not_first       = false;
 
     loop {
-        {// limit the scope of the .peek() so i can borrow .next()
-            let next = peekers.peek();
+        {// limit the scope of the .peek() so i can borrow w/ .next()
+            let next = coll.peek();
             if  next.is_some() && not_first {
                 zs.push("\t ".to_string());
             } else {
                 not_first = true;
             }
         }
-        match peekers.next() {
+        match coll.next() {
             None    => { break },
             Some(v) => { zs.push(v.to_string()); }
         }
