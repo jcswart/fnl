83dfb9b25f0db2a1bf9f410fdd6a793f4e097e47
Jarrod Swart <jcswart@gmail.com>
2017-02-10 19:54:19 -0500

    Factor our major logic, add basic usage.

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,43 @@ use std::io;
 use std::io::BufRead;
 
 fn main() {
-    let args: Vec<String>  = e::args().skip(1).collect();
-    let stdin = io::stdin();
+    let args: Vec<String> = e::args().skip(1).collect();
+    let stdin             = io::stdin();
     for l in stdin.lock().lines() {
-        let x = l.unwrap();
-        if x == "".to_string() {
+        let line = l.unwrap();
+        if line == "".to_string() {
             println!("");
         } else {
-            let pieces: Vec<&str> = x.split_whitespace().collect();
-            let mut output = vec![];
+            let pieces: Vec<&str> = line.split_whitespace().collect();
             if args.len() <= 0 {
-                let y: Vec<String> = pieces.iter().map(|s| String::from(*s)).collect();
-                println!("{}", intersperse(y, "\t".to_string()).into_iter().collect::<String>());
+                print_all(pieces);
+            } else if args.len() == 1 && args[0] == "-h".to_string() {
+                print_help();
             } else {
-                for i in &args {
-                    let j = i.parse::<u32>().unwrap() - 1;
-                    output.push(pieces.get(j as usize).unwrap());
-                }
-                let z: Vec<String> = output.iter().map(|s| String::from(**s)).collect();
-                println!("{}", intersperse(z, "\t".to_string()).into_iter().collect::<String>());
+                print_selected(&args, pieces);
             }
         }
     }
+}
+
+fn print_selected(args: &Vec<String>, pieces: Vec<&str>) -> () {
+    let mut output = vec![];
+    for i in args {
+        let j = i.parse::<u32>().unwrap() - 1;
+        output.push(pieces.get(j as usize).unwrap());
+    }
+    let z: Vec<String> = output.iter().map(|s| String::from(**s)).collect();
+    println!("{}", intersperse(z, "\t".to_string()).into_iter().collect::<String>());
+}
+
+fn print_all(pieces: Vec<&str>) -> () {
+    let y: Vec<String> = pieces.iter().map(|s| String::from(*s)).collect();
+    println!("{}", intersperse(y, "\t".to_string()).into_iter().collect::<String>());
+}
+
+fn print_help() -> ! {
+    println!("Usage: fnl [COLUMNS...]\n\nCOLUMNS\tOptional column numbers to select.\n\nExamples:\n\n\tex:     echo 'a     b c' | fnl 2 1\n\toutput: b\ta\n\n\tex:     echo 'a     b c' | fnl \n\toutput: a\tb\tc");
+    std::process::exit(1);
 }
 
 fn intersperse(xs: Vec<String>, seperator: String) -> Vec<String> {
