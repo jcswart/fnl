a85c58ad39be70901f911b62493cb5c27df7e2c3
Jarrod Swart <jcswart@gmail.com>
2017-12-29 13:06:33 -0500

    Use clap instead of ad hoc arg parsing. Add delimiter support.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
+#[macro_use]
+extern crate clap;
+
 use std::env as e;
 use std::io;
 use std::io::BufRead;
 
-fn main() {
-    let args: Vec<String> = e::args().skip(1).collect();
-    let stdin             = io::stdin();
-    if args.len() == 1 && (args[0] == "-h" ||
-                           args[0] == "--help") {
-        print_help();
-    }
+use clap::App;
 
+
+fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let delim: &str = matches.value_of("delimiter").unwrap_or("\t");
+    let columns: Vec<&str> = matches.values_of("columns").unwrap_or(clap::Values::default()).collect();
+
+    let stdin = io::stdin();
     for l in stdin.lock().lines() {
         let line = l.unwrap();
         if line == "" {
             println!();
         } else {
-            let pieces: Vec<&str> = line.split_whitespace().collect();
-            if args.is_empty() {
+            let pieces: Vec<&str> = line.split(delim).collect();
+            if columns.is_empty() {
                 print_all(&pieces);
             } else {
-                print_selected(&args, &pieces);
+                print_selected(&columns, &pieces);
             }
         }
     }
 }
 
-fn print_selected(args: &[String], pieces: &[&str]) -> () {
+fn print_selected(columns: &Vec<&str>, pieces: &[&str]) -> () {
     let mut output = vec![];
-    for i in args {
+    for i in columns {
         let j = i.parse::<u32>().unwrap() - 1;
         output.push(&pieces[j as usize]);
     }
@@ -38,11 +44,6 @@ fn print_selected(args: &[String], pieces: &[&str]) -> () {
 fn print_all(pieces: &[&str]) -> () {
     let y: Vec<String> = pieces.iter().map(|s| String::from(*s)).collect();
     println!("{}", intersperse(&y, &"\t".to_string()).into_iter().collect::<String>());
-}
-
-fn print_help() -> ! {
-    println!("Usage: fnl [COLUMNS...]\n\nCOLUMNS\tOptional column numbers to select.\n\nExamples:\n\n\tex:     echo 'a     b c' | fnl 2 1\n\toutput: b\ta\n\n\tex:     echo 'a     b c' | fnl \n\toutput: a\tb\tc");
-    std::process::exit(1);
 }
 
 fn intersperse<'a>(xs: &'a [String], seperator: &'a str) -> Vec<&'a str> {
